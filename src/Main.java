cd5fa69069f66b59ce13512fd1939d7d7d4593de
aashritmathur24 <31812971+aashritmathur24@users.noreply.github.com>
2017-09-12 12:59:21 +0530

    First

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		/*Student stu1=new Student(1,"Aashrit Mathur","1996-02-24",88.88);
+		Student stu2=new Student(2,"Rohit Sharma","1989-02-24",83.47);
+		Student stu3=new Student(3,"Virat Kohli","1990-02-24",76.91);
+		StudentGroup sg=new StudentGroup(3);
+		sg.getStudents();
+		sg.setStudents(ob1);
+		sg.getStudent(1);*/
+	}
+}
